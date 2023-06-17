cc86771e2fa42815cf350f9a4a97bd48ce5fe682
MohitVishwakar <mohitviswakrama78@gmail.com>
2023-06-17 22:30:35 +0530

    contest code

---
+++ INDORE.html/javam.java
@@ -0,0 +1,12 @@
+import java.util.*;
+    class assingment{
+         public static void main(String[] args) {
+            Scanner sc= new Scanner (System.in);
+            String name= sc.nextLine();
+            int age= sc.nextInt();
+
+            System.out.print("My name is "+ name+ ". ");
+            System.out.println("I am "+ age + " years old !");
+         }
+
+         }
---
+++ projects.java/one.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import org.w3c.dom.Text;
+
+class one{
+ public static void main(String[] args) {
+    System.out.println("MOHIT WELCOME AGAIN");
+    Scanner sc = new Scanner(System.in);
+    String name= sc.nextLine();
+      int a=sc.nextInt();
+      int b=sc.nextInt(); 
+      int sume= a+b;
+      System.out.println(sume);
+      System.out.println(name);
+      
+      
+     
+
+
+}
+}
